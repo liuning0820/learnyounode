0db99f058d7249f43ae8c639620ed97de88080a9
Toby,Liu <ning.liu4@hp.com>
2015-07-28 15:08:43 +0800

    add sample

--- learnyounode.js
+++ learnyounode.js
@@ -250,28 +250,41 @@ To install through2-map type:
 /* Lesson 13 HTTP JSON API SERVER
 */
 
-var http = require('http')
-var url = require('url')
-http.createServer(function(inStream, outStream) {
-var result = '';
-var d = '';
-var urlObject = url.parse(inStream.url, true);
-if(urlObject.pathname === '/api/parsetime') {
-	result = urlObject.query.iso;
-	d = new Date(result);
-	result = {"hour": d.getHours(), "minute": d.getMinutes(), "second": d.getSeconds()};
-}
-else if (urlObject.pathname === '/api/unixtime') {
-	result = urlObject.query.iso;
-	d = new Date(result);
-	result = {"unixtime": d.getTime()};
-}
-if (result) {
-    outStream.writeHead(200, { 'Content-Type': 'application/json' });
-    outStream.end(JSON.stringify(result));
-	}
-	else {
-	outStream.writeHead(404);
-	  	outStream.end();
-	}
-}).listen(process.argv[2]);
+// var http = require('http')
+// var url = require('url')
+// http.createServer(function(inStream, outStream) {
+// var result = '';
+// var d = '';
+// var urlObject = url.parse(inStream.url, true);
+// if(urlObject.pathname === '/api/parsetime') {
+// 	result = urlObject.query.iso;
+// 	d = new Date(result);
+// 	result = {"hour": d.getHours(), "minute": d.getMinutes(), "second": d.getSeconds()};
+// }
+// else if (urlObject.pathname === '/api/unixtime') {
+// 	result = urlObject.query.iso;
+// 	d = new Date(result);
+// 	result = {"unixtime": d.getTime()};
+// }
+// if (result) {
+//     outStream.writeHead(200, { 'Content-Type': 'application/json' });
+//     outStream.end(JSON.stringify(result));
+// 	}
+// 	else {
+// 	outStream.writeHead(404);
+// 	  	outStream.end();
+// 	}
+// }).listen(process.argv[2]);
+
+
+/* Code 大熊君大话NodeJS之开篇 */
+
+var http = require('http');
+http.createServer(function(requst, response){
+	response.writeHead(200,{
+		"Content-Type": "text/plain"
+	});
+
+	response.write("Hello, DaXiong");
+	response.end();
+}).listen(8888);
